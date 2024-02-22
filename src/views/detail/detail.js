315865053fcfe8336d8deb4143ee418c306a6a78
k65860 <jy_0327@likelion.org>
2024-02-23 00:24:01 +0900

    🔨 Chore: Add FE project folders and files
    detail, list파일의 html과 js 수정

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -1,39 +1,32 @@
-// burger
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active');
-});
-
-function count(type)  {
+// eslint-disable-next-line no-unused-vars
+function count(type) {
   // 결과를 표시할 element
   const resultElement = document.getElementById('result');
-  
+
   // 현재 화면에 표시된 값
   let number = resultElement.innerText;
-  
+
   // 더하기/빼기
-  if(type === 'plus') {
-    number = parseInt(number) + 1;
+  if (type === 'plus') {
+    number = parseInt(number, 10) + 1;
+  } else if (type === 'minus') {
+    if (number >= 2) {
+      number = parseInt(number, 10) - 1;
+    } else {
+      number = 1;
+    }
   }
-  else if(type === 'minus')  {
-      if(number >= 1) {
-          number = parseInt(number) - 1;
-      }
-      else {
-          number = 0;
-      }
-  }
-  
+
   // 결과 출력
   resultElement.innerText = number;
 }
 
-function cartBtn()  {
-  window.location.href = '/list/list.html';
+// eslint-disable-next-line no-unused-vars
+function cartBtn() {
+  window.location.href = '/list.html';
 }
 
-function payBtn()  {
-  window.location.href = '결제완료url';
+// eslint-disable-next-line no-unused-vars
+function payBtn() {
+  window.location.href = '/order.html';
 }
