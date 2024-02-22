a3406e5b6df4a50fdd9d761b4b184424b37c817c
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-02-22 17:12:09 +0900

    🔨 Chore: Add project folders and files
    src 폴더에 app.js 파일 위치 변경
    src 폴더에 controllers, db, middlewares, services 폴더 및 파일 추가

---
+++ src/db/models/index.js
@@ -0,0 +1,13 @@
+const User = require('./user');
+const Product = require('./product');
+const Order = require('./order');
+const Category = require('./category');
+const Delivery = require('./delivery');
+
+module.exports = {
+  User,
+  Product,
+  Order,
+  Category,
+  Delivery,
+};
---
+++ src/db/schemas/index.js
@@ -0,0 +1,13 @@
+const UserSchema = require('./userSchema');
+const ProductSchema = require('./productSchema');
+const OrderSchema = require('./orderSchema');
+const CategorySchema = require('./categorySchema');
+const DeliverySchema = require('./deliverySchema');
+
+module.exports = {
+  UserSchema,
+  ProductSchema,
+  OrderSchema,
+  CategorySchema,
+  DeliverySchema,
+};
