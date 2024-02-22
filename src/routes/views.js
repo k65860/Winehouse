44a8ff96c4bf25b9898282fbc2a0a43368cf4989
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-02-22 20:43:03 +0900

    🔨 Chore: Add FE project folders and files
    views 폴더에 페이지별 폴더 생성

---
+++ src/routes/views.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+const path = require('path');
+
+// views폴더 내의 ${resource} 폴더 내의 모든 파일을 웹에 띄우며,
+// 이 때 ${resource}.html 을 기본 파일로 설정함.
+function serveStatic(resource) {
+  const resourcePath = path.join(__dirname, `../views/${resource}`);
+  const option = { index: `${resource}.html` };
+
+  return express.static(resourcePath, option);
+}
+
+router.use('/', serveStatic('home'));
+router.use('/list', serveStatic('list'));
+router.use('/detail', serveStatic('detail'));
+router.use('/cart', serveStatic('cart'));
+router.use('/order', serveStatic('order'));
+
+module.exports = router;
