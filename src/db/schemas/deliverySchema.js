3bec3d9895517281d51d2f75782402e988d60cdc
Jueun Lee <leeje7766@gmail.com>
2024-02-23 14:26:20 +0900

    ✨Feat: Add delivery schema file

---
+++ src/db/schemas/deliverySchema.js
@@ -0,0 +1,41 @@
+const { Schema } = require('mongoose');
+
+const DeliverySchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+
+    orderId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+
+    deliveryStatus: {
+      type: Number,
+      required: true,
+    },
+
+    deliveryDate: {
+      type: Date,
+    },
+
+    deliveryWish: {
+      type: String,
+    },
+  },
+
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+
+    deletedAt: {
+      type: Date,
+    },
+  }
+);
+
+module.exports = DeliverySchema;
