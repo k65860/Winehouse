1ba4f51638f9b8197fd9887e891f27f3dd113655
2hani <155037923+2hani@users.noreply.github.com>
2024-02-25 01:50:45 +0900

    🔨 Feat:Amend update page
    정보 수정 페이지 코드 수정

--- src/views/update/update.js
+++ src/views/update/update.js
@@ -4,12 +4,12 @@ function EditProfile() {
   }
 
   function confirmWithdraw() {
-    var confirmResult = confirm("정말 탈퇴하시겠습니까?");
+    const confirmResult = confirm("정말 탈퇴하시겠습니까?");
 
     if (confirmResult) {
         // '예'를 선택한 경우
         alert("탈퇴 되었습니다.")
-        window.location.href = "http://localhost:4000/"; 
+        window.location.href = "/"; 
     } else {
         // '아니요'를 선택한 경우
         alert("탈퇴가 취소되었습니다.");
