df15ad78c4e75ef82fd277d29e2138d3e0d406e0
2hani <155037923+2hani@users.noreply.github.com>
2024-02-24 21:55:11 +0900

    ✨ Feat:Add update page
    정보수정 페이지 구현

---
+++ src/views/update/update.js
@@ -0,0 +1,17 @@
+function EditProfile() {
+    alert("정보수정 되었습니다.");
+
+  }
+
+  function confirmWithdraw() {
+    var confirmResult = confirm("정말 탈퇴하시겠습니까?");
+
+    if (confirmResult) {
+        // '예'를 선택한 경우
+        alert("탈퇴 되었습니다.")
+        window.location.href = "http://localhost:4000/"; 
+    } else {
+        // '아니요'를 선택한 경우
+        alert("탈퇴가 취소되었습니다.");
+    }
+}
