ad2009362c6ab0819008834b4fd73430bfe33c74
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-02-26 00:44:19 +0900

    🔨 Chore: Change cart page plus/minus button
    - input type button 에서 button 태그로 수정
    - 돔에 직접 함수를 주입하는 부분 수정

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 function count(type, productIndex) {
   // 결과를 표시할 element
-  const resultElements = document.querySelectorAll('#result');
+  const resultElements = document.querySelectorAll('#quantity');
   const resultElement = resultElements[productIndex - 1];
 
   // 현재 화면에 표시된 값
@@ -23,19 +23,21 @@ function count(type, productIndex) {
   resultElement.innerText = number;
 }
 
-const minusButtons = document.querySelectorAll('#quantityBtnMinus');
-const plusButtons = document.querySelectorAll('#quantityBtnPlus');
+document.addEventListener('DOMContentLoaded', () => {
+  const minusButtons = document.querySelectorAll('#quantityBtnMinus');
+  const plusButtons = document.querySelectorAll('#quantityBtnPlus');
 
-// 마이너스 버튼에 리스너 추가
-minusButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    count('minus', index); // 해당 상품의 인덱스를 전달하여 수량 조절
+  // 마이너스 버튼
+  minusButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      count('minus', index + 1); // 해당 상품의 인덱스를 전달하여 수량 조절
+    });
   });
-});
 
-// 플러스 버튼에 리스너 추가
-plusButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    count('plus', index); // 해당 상품의 인덱스를 전달하여 수량 조절
+  // 플러스 버튼
+  plusButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      count('plus', index + 1); // 해당 상품의 인덱스를 전달하여 수량 조절
+    });
   });
 });
