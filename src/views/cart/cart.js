a45eb605cb45b01b6b40acf82ea3872428f1bca3
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-02-26 14:10:56 +0900

    ✨ Feat: Add cart product output
    - indexedDB에 저장된 장바구니 상품 출력 기능 추가
    - 변수명, 클래스명 수정

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,43 +1,113 @@
-// +, - 버튼
-// eslint-disable-next-line no-unused-vars
-function count(type, productIndex) {
-  // 결과를 표시할 element
-  const resultElements = document.querySelectorAll('#quantity');
-  const resultElement = resultElements[productIndex - 1];
+// 수량 조절 함수
+function adjustProductQuantity(type, productIndex) {
+  const quantityElements = document.querySelectorAll('.quantity');
+  const quantityElement = quantityElements[productIndex];
 
-  // 현재 화면에 표시된 값
-  let number = resultElement.innerText;
+  let currentQuantity = parseInt(quantityElement.innerText);
 
-  // 더하기/빼기
   if (type === 'plus') {
-    number = parseInt(number, 10) + 1;
+    currentQuantity += 1;
   } else if (type === 'minus') {
-    if (number >= 2) {
-      number = parseInt(number, 10) - 1;
+    if (currentQuantity >= 2) {
+      currentQuantity -= 1;
     } else {
-      number = 1;
+      currentQuantity = 1;
     }
   }
 
-  // 결과 출력
-  resultElement.innerText = number;
+  quantityElement.innerText = currentQuantity;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const minusButtons = document.querySelectorAll('#quantityBtnMinus');
-  const plusButtons = document.querySelectorAll('#quantityBtnPlus');
+const cartContainer = document.querySelector('#cart-items');
 
-  // 마이너스 버튼
-  minusButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      count('minus', index + 1); // 해당 상품의 인덱스를 전달하여 수량 조절
-    });
-  });
-
-  // 플러스 버튼
-  plusButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      count('plus', index + 1); // 해당 상품의 인덱스를 전달하여 수량 조절
-    });
-  });
+// 플러스 버튼
+cartContainer.addEventListener('click', (event) => {
+  if (event.target && event.target.classList.contains('quantity-plus-btn')) {
+    const index = Array.from(
+      cartContainer.querySelectorAll('.quantity-plus-btn')
+    ).indexOf(event.target);
+    adjustProductQuantity('plus', index);
+  }
 });
+
+// 마이너스 버튼
+cartContainer.addEventListener('click', (event) => {
+  if (event.target && event.target.classList.contains('quantity-minus-btn')) {
+    const index = Array.from(
+      cartContainer.querySelectorAll('.quantity-minus-btn')
+    ).indexOf(event.target);
+    adjustProductQuantity('minus', index);
+  }
+});
+
+// 장바구니 기능
+if (window.indexedDB) {
+  const request = window.indexedDB.open('winehouse');
+  let db;
+
+  request.onupgradeneeded = (event) => {
+    db = event.target.result;
+
+    const objectStore = db.createObjectStore('cart', {
+      keyPath: 'id',
+      autoIncrement: true,
+    });
+    objectStore.createIndex('productImage', 'productImage', { unique: false });
+    objectStore.createIndex('productName', 'productName', { unique: false });
+    objectStore.createIndex('productPrice', 'productPrice', { unique: false });
+  };
+
+  request.onerror = (event) => {
+    console.log(event.target.errorCode);
+  };
+
+  request.onsuccess = (event) => {
+    db = event.target.result;
+    const transaction = db.transaction(['cart'], 'readonly');
+    const store = transaction.objectStore('cart');
+
+    const requestGetItems = store.getAll();
+    
+    requestGetItems.onsuccess = (event) => {
+      const cartItems = event.target.result;
+      // 장바구니 상품을 출력할 부분
+      const cartContainer = document.querySelector('#cart-items');
+
+      // 각 상품에 대한 카드 생성
+      cartItems.forEach((item) => {
+        const card = document.createElement('div');
+        card.classList.add('card', 'mb-5');
+
+        card.innerHTML = `
+          <div class="card-content">
+            <div class="content">
+              <div class="is-flex is-align-items-center">
+                <label class="checkbox">
+                  <input type="checkbox">
+                </label>
+                <div class="product-image m-2">
+                  <img src="${item.productImage}" alt="Product Image" />
+                </div>
+                <div class="product-info">
+                  <div class="product-name">
+                    <p>${item.productName}</p>
+                  </div>
+                  <div class="product-price">
+                    <p>${item.productPrice}원</p>
+                  </div>
+                </div>
+              </div>
+              <div class="count-button">
+                <button class="quantity-minus-btn">-</button>
+                <span class="quantity">1</span>
+                <button class="quantity-plus-btn">+</button>
+              </div>
+            </div>
+          </div>`;
+
+        // 카드를 컨테이너에 추가
+        cartContainer.appendChild(card);
+      });
+    };
+  };
+}
