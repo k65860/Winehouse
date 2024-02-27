3a6e27aaee0e13425b4723609a49fcfbc4b1e4b0
Hyunbin Jun <gosqlsz@gmail.com>
2024-02-27 06:46:33 +0000

    🐛 Bug : BE 1st feature merging bug fixing

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
 const createError = require('http-errors');
 const express = require('express');
 const mongoose = require('mongoose');
