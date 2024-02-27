3d7b6d7efd193faf897954fbe14d53442839ac02
Jueun Lee <leeje7766@gmail.com>
2024-02-27 23:29:30 +0900

    🔨 Fix: Add a new field 'name' to the User schema

--- src/db/schemas/userSchema.js
+++ src/db/schemas/userSchema.js
@@ -1,6 +1,10 @@
 const { Schema } = require('mongoose');
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
