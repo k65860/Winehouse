fd0736f00f148ca2288fc5481e364fa2ed0a846c
k65860 <jy_0327@likelion.org>
2024-02-28 12:24:34 +0900

    🔨 Chore: Change js for order/list page
    order에 alert 추가 / list에 확인 alert 추가

--- src/views/list/list.js
+++ src/views/list/list.js
@@ -3,9 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   photos.forEach((photo) => {
     photo.addEventListener("click", () => {
-      if (confirm("장바구니 페이지로 이동하시겠습니까?")) {
-        window.location.href = "/detail";
-      }
+      window.location.href = "/detail";
     });
   });
 });
