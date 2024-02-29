c3712a53590c0bd705697a2c9db8cfe808571711
k65860 <jy_0327@likelion.org>
2024-02-29 12:07:34 +0900

    ✨ Feat: list, detail page update(ing)
    list, detail page 수정 중

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -1,28 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-// function count(type) {
-//   // 결과를 표시할 element
-//   const resultElement = document.getElementById("result");
-//   const minusBtn = document.getElementById("minus");
-//   const plusBtn = document.getElementById("plus");
-
-//   // 현재 화면에 표시된 값
-//   let number = resultElement.innerText;
-
-//   // 더하기/빼기
-//   if (type === "plus") {
-//     number = parseInt(number, 10) + 1;
-//   } else if (type === "minus") {
-//     if (number >= 2) {
-//       number = parseInt(number, 10) - 1;
-//     } else {
-//       number = 1;
-//     }
-//   }
-
-//   // 결과 출력
-//   resultElement.innerText = number;
-// }
-
 document.addEventListener("DOMContentLoaded", function () {
   // 장바구니 버튼 클릭 시 alert
   document
@@ -43,33 +18,63 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
-  // 마이너스 플러스 버튼
-  const resultElement = document.getElementById("result");
-  let resultValue = parseInt(resultElement.innerText);
-
-  document.getElementById("plus").addEventListener("click", function () {
-    // resultValue 값을 1씩 증가시킵니다.
-    resultValue++;
-    resultElement.innerText = resultValue;
-  });
-
-  document.getElementById("minus").addEventListener("click", function () {
-    // resultValue 값이 1보다 큰 경우에만 감소
-    if (resultValue > 1) {
-      resultValue--;
-      resultElement.innerText = resultValue;
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  // 장바구니 버튼
-  document.getElementById("cartBtn").addEventListener("click", function () {
-    window.location.href = "/cart";
-  });
-
   // 결제하기 버튼
   document.getElementById("payBtn").addEventListener("click", function () {
     window.location.href = "/order";
   });
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const res = await fetch("/product");
+    const data = await res.json();
+    console.log(data);
+
+    const products = data.data.reverse().slice(0, 5); // 최신 5개 상품만
+    console.log(products);
+
+    // 0번째 요소의 product_name 값 불러오기
+    const product_name =
+      products.length > 0 ? products[0].product_name : "No data"; // 데이터가 없으면 'No data'를 표시
+
+    // id가 'data-name'인 a 태그에 상품 type 추가
+    const dataNameElement = document.getElementById("data-name");
+    dataNameElement.textContent = `${product_name}`;
+
+    // 0번째 요소의 product_price 값 불러오기
+    const product_price =
+      products.length > 0 ? products[0].product_price : "No data"; // 데이터가 없으면 'No data'를 표시
+
+    const dataPriceElement = document.getElementById("data-price");
+    dataPriceElement.textContent = `${product_price}`;
+
+    // 마이너스 플러스 버튼
+    const resultElement = document.getElementById("result");
+    let resultValue = parseInt(resultElement.innerText);
+
+    document.getElementById("plus").addEventListener("click", function () {
+      // resultValue 값을 1씩 증가시킵니다.
+      resultValue++;
+      resultElement.innerText = resultValue;
+
+      // 총 가격
+      let totalPriceElement = document.getElementById("data-totalPrice");
+      let resultValueElement = parseInt(resultValue);
+      totalPriceElement.textContent = `${resultValueElement * product_price}`;
+    });
+
+    document.getElementById("minus").addEventListener("click", function () {
+      // resultValue 값이 1보다 큰 경우에만 감소
+      if (resultValue > 1) {
+        resultValue--;
+        resultElement.innerText = resultValue;
+      }
+      // 총 가격
+      let totalPriceElement = document.getElementById("data-totalPrice");
+      let resultValueElement = parseInt(resultValue);
+      totalPriceElement.textContent = `${resultValueElement * product_price}`;
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
--- src/views/list/list.js
+++ src/views/list/list.js
@@ -7,3 +7,61 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const res = await fetch("/product");
+    //data json형식으로 불러오기
+    const data = await res.json();
+    // console.log(data);
+
+    const products = data.data.slice(0, 3); // 최신 5개 상품만
+    // console.log(products);
+
+    const productsContainer = document.querySelector('.main');
+    productsContainer.innerHTML = ''; // 기존 상품 목록 초기화
+
+    // products 배열을 순회하면서 각 상품의 정보를 dataRowDiv에 추가합니다.
+    products.forEach((product) => {
+      const productElement = document.createElement('div');
+      productElement.classList.add('productPhoto1');
+
+      productElement.innerHTML += `
+        <div class="photoBox">
+          <img src="https://cdn.pixabay.com/photo/2018/01/12/09/45/glass-3077869_1280.jpg" id="photo">
+            <div class="dataBox">
+              <div class="dataRow">
+                <a id="data-name">${product.product_name}</a>
+              </div>
+              <div class="dataRow">
+                <a id="data-price">${product.product_price}</a>
+              </div>
+            </div>
+        </div>
+    `;
+    productsContainer.appendChild(productElement);
+
+      // dataNameElement.textContent = product.product_name;
+      // dataRowDiv.appendChild(dataNameElement);
+
+      // // data-price에 product_price 추가
+      // const dataPriceElement = document.createElement("a");
+      // dataPriceElement.id = "data-price";
+      // dataPriceElement.textContent = product.product_price;
+      // dataRowDiv.appendChild(dataPriceElement);
+    });
+
+    // // id가 'data-name'인 a 태그에 상품 type 추가
+    // const dataNameElement = document.getElementById("data-name");
+    // dataNameElement.textContent = `${product_name}`;
+
+    // // 0번째 요소의 product_price 값 불러오기
+    // const product_price = products.length > 0 ? products[0].product_price : "No data"; // 데이터가 없으면 'No data'를 표시
+
+    // const dataPriceElement = document.getElementById("data-price");
+
+    // dataPriceElement.textContent = `${product_price}`;
+  } catch (err) {
+    console.log(err);
+  }
+});
