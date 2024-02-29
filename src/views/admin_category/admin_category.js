692577763c9a73ad60059bb2ccc8be306a75a585
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-02-29 23:30:20 +0900

    ✨ Feat: category add admin-category page
    관리자 카테고리 관리 페이지에서 카테고리 추가 구현
    (이미 존재하는 카테고리일 경우 alert)

--- src/views/admin_category/admin_category.js
+++ src/views/admin_category/admin_category.js
@@ -24,22 +24,69 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
 });
 
+// 카테고리 추가
+document.addEventListener('click', async (e) => {
+  if (e.target && e.target.id === 'addBtn') {
+    const modal = document.querySelector('#categoryAddModal');
+    modal.classList.add('is-active');
+
+    const categoryNameInput = document.querySelector('#categoryName');
+    const categoryModifyBtn = document.querySelector('#categoryAddBtn');
+
+    // 모달에서 완료 버튼 클릭
+    categoryModifyBtn.addEventListener('click', async () => {
+      const categoryName = categoryNameInput.value;
+      if (!categoryName) {
+        return alert('카테고리 이름을 입력해 주세요.');
+      }
+
+      try {
+        const res = await fetch('/category', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ categoryName: categoryName }),
+        });
+        const data = await res.json();
+
+        if (data.status === 500) {
+          alert('이미 존재하는 카테고리입니다.');
+        }
+
+        // alert('카테고리가 추가되었습니다.');
+        modal.classList.remove('is-active');
+        window.location.reload();
+      } catch (err) {
+        console.error(err);
+      }
+    });
+
+    // 모달 취소 버튼 클릭 or 모달 배경 클릭
+    modal.addEventListener('click', (e) => {
+      if (e.target.classList.contains('modal-background') || e.target.id === 'modalCancelBtn') {
+        modal.classList.remove('is-active');
+      }
+    });
+  }
+});
+
 // 카테고리 수정
 document.addEventListener('click', async (e) => {
   if (e.target && e.target.id === 'updateBtn') {
     const categoryId = e.target.name;
-    const modal = document.querySelector('#categoryModal');
+    const modal = document.querySelector('#categoryModifyModal');
     modal.classList.add('is-active');
 
     const newCategoryNameInput = document.querySelector('#newCategoryName');
-    const saveBtn = document.querySelector('#saveBtn');
+    const categoryModifyBtn = document.querySelector('#categoryModifyBtn');
 
     // 기존 카테고리 이름
     const categoryName = e.target.parentElement.parentElement.querySelector('.item').textContent;
     newCategoryNameInput.value = categoryName;
 
     // 모달에서 완료 버튼 클릭
-    saveBtn.addEventListener('click', async () => {
+    categoryModifyBtn.addEventListener('click', async () => {
       const newCategoryName = newCategoryNameInput.value;
       if (!newCategoryName) {
         return alert('카테고리 이름을 입력해 주세요.');
