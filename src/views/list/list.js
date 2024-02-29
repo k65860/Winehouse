7009621efeb562e1d012206313b26dcd34f98f3d
k65860 <jy_0327@likelion.org>
2024-02-29 14:37:44 +0900

    ✨  Feat: list, detail page update(ing)
    list, detail page update

--- src/views/list/list.js
+++ src/views/list/list.js
@@ -15,19 +15,18 @@ document.addEventListener("DOMContentLoaded", async () => {
     const data = await res.json();
     // console.log(data);
 
-    const products = data.data.slice(0, 3); // 최신 5개 상품만
-    // console.log(products);
+    const products = data.data;
+    console.log(products);    
 
-    const productsContainer = document.querySelector('.main');
+    const productsContainer = document.querySelector('.productPhoto1');
     productsContainer.innerHTML = ''; // 기존 상품 목록 초기화
 
     // products 배열을 순회하면서 각 상품의 정보를 dataRowDiv에 추가합니다.
     products.forEach((product) => {
       const productElement = document.createElement('div');
-      productElement.classList.add('productPhoto1');
+      productElement.classList.add('photoBox');
 
       productElement.innerHTML += `
-        <div class="photoBox">
           <img src="https://cdn.pixabay.com/photo/2018/01/12/09/45/glass-3077869_1280.jpg" id="photo">
             <div class="dataBox">
               <div class="dataRow">
@@ -37,10 +36,10 @@ document.addEventListener("DOMContentLoaded", async () => {
                 <a id="data-price">${product.product_price}</a>
               </div>
             </div>
-        </div>
     `;
     productsContainer.appendChild(productElement);
 
+    
       // dataNameElement.textContent = product.product_name;
       // dataRowDiv.appendChild(dataNameElement);
 
