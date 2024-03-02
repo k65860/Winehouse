95f25c475050ec52d896c786ed2d54f33f56bd69
k65860 <jy_0327@likelion.org>
2024-03-02 12:24:54 +0900

    ✨ Feat: admin_order api update
    admin_order img api update

--- src/views/list/list.js
+++ src/views/list/list.js
@@ -1,23 +1,51 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  // 카테고리 가져오기
+  try {
+    const urlParams = new URLSearchParams(window.location.search);
+    const categoryId = urlParams.get('categoryId');
+
+    const res = await fetch('/category');
+    const data = await res.json();
+
+    if (data.status !== 200) {
+      throw new Error('카테고리 목록을 가져오는데 실패했습니다.');
+    }
+
+    const categories = data.data;
+
+    const categoryElement = document.querySelector('.category-container');
+
+    categories.forEach((category) => {
+      if (category._id === categoryId)
+      categoryElement.innerHTML += `
+          <p class="listFont">
+            <i class="fa-solid fa-wine-bottle"></i> 
+            <span class="category-name">${category.category_name} 와인</span>
+          </p>
+        `;
+    });
+  } catch (error) {
+    console.error('카테고리 불러오기 오류:', error);
+  }
+
+  // 카테고리별 상품 가져오기
   try {
     const urlParams = new URLSearchParams(window.location.search);
     const categoryId = urlParams.get('categoryId');
     
     const res = await fetch(`/product/${categoryId}`);
-    //data json형식으로 불러오기
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
 
     const products = data.data;
-    console.log(products);
+    // console.log(products);
 
-    const productsContainer = document.querySelector('.productPhoto1');
+    const productsContainer = document.querySelector('.productPhoto');
     productsContainer.innerHTML = ''; // 기존 상품 목록 초기화
 
     // products 배열을 순회하면서 각 상품의 정보를 dataRowDiv에 추가합니다.
     products.forEach(async product => {
       const imageId = product.image_id;
-      console.log(imageId);
 
       const imgRes = await fetch(`/product/image/${imageId}`);
       const imgData = await imgRes.json();
@@ -28,31 +56,20 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       productElement.innerHTML += `
         <a href="/detail?productId=${product._id}">
-          <span id="photo">${imgSrc}</span>
+          ${imgSrc}
         </a>
             <div class="dataBox">
               <div class="dataRow">
                 <a id="data-name">${product.product_name}</a>
               </div>
               <div class="dataRow">
-                <a id="data-price">${product.product_price}</a>
+                <a id="data-price">${product.product_price} 원</a>
               </div>
             </div>
     `;
     productsContainer.appendChild(productElement);
     });
-   
   } catch (err) {
     console.log(err);
   }
 });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const photos = document.querySelectorAll("#photo");
-
-//   photos.forEach((photo) => {
-//     photo.addEventListener("click", () => {
-//       window.location.href = `${product.product_id}`;
-//     });
-//   });
-// });
